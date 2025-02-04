37c53461b67203b067f70d193077c1d214976571
Dhyey Pithadia <dhyeypithadia@gmail.com>
2025-02-04 11:18:16 +0530

    3rd commit

--- 230470107140/P4/P4.java
+++ 230470107140/P4/P4.java
@@ -1,70 +1,40 @@
-class Member {
+class Employee { 
     String name;
     int age;
-    String phoneNumber;
+    String phone;
     String address;
     double salary;
-
-    // Constructor
-    public Member(String name, int age, String phoneNumber, String address, double salary) {
-        this.name = name;
-        this.age = age;
-        this.phoneNumber = phoneNumber;
-        this.address = address;
-        this.salary = salary;
-    }
-
-    // Method to print salary
-    public void printSalary() {
-        System.out.println("Salary: " + salary);
-    }
-
-    // Method to display member details
-    public void displayDetails() {
-        System.out.println("Name: " + name);
-        System.out.println("Age: " + age);
-        System.out.println("Phone Number: " + phoneNumber);
-        System.out.println("Address: " + address);
-        printSalary();
-    }
-}
-
-// Employee class inherits from Member
-class Employee extends Member {
-    String specialization;
-
-    // Constructor
-    public Employee(String name, int age, String phoneNumber, String address, double salary, String specialization) {
-        super(name, age, phoneNumber, address, salary);
-        this.specialization = specialization;
-    }
-
-    // Display Employee details
-    public void displayEmployeeDetails() {
-        displayDetails();
-        System.out.println("Specialization: " + specialization);
-        System.out.println("---------------------------------");
-    }
-}
-
-// Manager class inherits from Member
-class Manager extends Member {
     String department;
 
-    // Constructor
-    public Manager(String name, int age, String phoneNumber, String address, double salary, String department) {
-        super(name, age, phoneNumber, address, salary);
+    public Employee(String name, int age, String phone, String address, double salary, String department) {
+        this.name = name;
+        this.age = age;
+        this.phone = phone;
+        this.address = address;
+        this.salary = salary;
         this.department = department;
     }
 
-    // Display Manager details
-    public void displayManagerDetails() {
-        displayDetails();
+    public void displayEmployeeDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Phone: " + phone);
+        System.out.println("Address: " + address);
+        System.out.println("Salary: " + salary);
         System.out.println("Department: " + department);
         System.out.println("---------------------------------");
     }
 }
 
+class Manager extends Employee {
+    public Manager(String name, int age, String phone, String address, double salary, String department) {
+        super(name, age, phone, address, salary, department);
+    }
+
+    public void displayManagerDetails() {
+        displayEmployeeDetails();
+    }
+}
 
 public class P4 {
     public static void main(String[] args) {
@@ -81,5 +51,4 @@ public class P4 {
         System.out.println("Manager Details:");
         mgr.displayManagerDetails();
     }
-
 }
